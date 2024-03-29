22a4b7f05e63b73a65fd57eeef3ab2c85a5da05e
jdang90 <112575581+jdang90@users.noreply.github.com>
2024-03-28 22:50:12 -0700

    Update StudentList.cpp

--- Grade-Report-VS/Project 2/StudentList.cpp
+++ Grade-Report-VS/Project 2/StudentList.cpp
@@ -1,12 +1,5 @@
 ﻿/*
-	Nguyen, An (Team Leader)
-	Nguyen, Justin
-	Chen, Shanqin
-	Dang, Justin
-
-	Project: Grade Report
-	CS A250
-	Fall 2023
+	Creates output for a student's information.
 */
 
 #include "StudentList.h"
