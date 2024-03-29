73fbc6ddee5f36942a2f1c60dcc56ebdcdae9653
jdang90 <112575581+jdang90@users.noreply.github.com>
2024-03-28 22:42:28 -0700

    Update OutputHandler.cpp

--- Grade-Report-VS/Project 2/OutputHandler.cpp
+++ Grade-Report-VS/Project 2/OutputHandler.cpp
@@ -1,12 +1,5 @@
 /*
-	Nguyen, An (Team Leader)
-	Nguyen, Justin
-	Chen, Shanqin
-	Dang, Justin
-
-	Project: Grade Report
-	CS A250
-	Fall 2023
+	Handles the output for Main.cpp
 */
 
 #include "OutputHandler.h"
